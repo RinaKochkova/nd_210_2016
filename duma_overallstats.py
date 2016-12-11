16579394544930a9f37ed21476754e9402daa9f0
chtoproishodit7 <zuk_kalinina@mail.ru>
2016-12-11 20:12:41 +0500

    это более прилично выглядит чем предыдущий скрипт

--- duma_overallstats.py
+++ duma_overallstats.py
@@ -4,38 +4,23 @@ connection = sqlite3.connect('duma.db')
 cursor = connection.cursor()
 
 cursor.execute('SELECT vote FROM votes')
-overall_stat = []
+vote =[]
 for row in cursor.fetchall():
-    overall_stat.append(row)
-
-accept_per = 0
-denied_per = 0
-refrain_per = 0
-absent_per = 0
-appearance_per = 0
-for row in overall_stat:
-    if row[0] == 'none':
-        absent_per = absent_per + 1
-    elif row[0] == 'accept':
-        accept_per = accept_per + 1
-    elif row[0] == 'decline':
-        denied_per = denied_per + 1
-    elif row[0] == 'abstain':
-        refrain_per = refrain_per +1
-appearance = len(overall_stat) - absent_per
-accept_per = accept_per / appearance
-denied_per = denied_per / appearance
-refrain_per = refrain_per/appearance
-absent_per =  absent_per/ len(overall_stat)
-appearance_per = appearance / len(overall_stat)
-
+    vote.append(row[0])
+absent = vote.count('none')
+all =len(vote)
+acc = vote.count('accept')
+den = vote.count('decline')
+ref = vote.count('abstain')
+app = all -absent
 cursor.close()
 connection.close()
 
 connection = sqlite3.connect('db.sqlite3')
 cursor = connection.cursor()
-data = [1,appearance,accept_per,denied_per,refrain_per,absent_per,appearance_per]
-cursor.execute('INSERT INTO duma_overallstats VALUES (?,?,?,?,?,?,?)',data)
+stat = [1,app,acc/app,den/app,ref/app,absent/all,app/all]
+cursor.execute('INSERT INTO duma_overallstats  VALUES ( ?,?,?,?,?,?,?)',stat)
 connection.commit()
 cursor.close()
 connection.close()
+
